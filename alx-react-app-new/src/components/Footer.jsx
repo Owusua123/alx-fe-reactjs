a39cf86c0639abc8eb23719c1f83a6f49b0a2734
Nana Ama Sarpong <nanaamasarpong824@gmail.com>
2025-07-20 14:29:15 +0000

    Adding Inline css styling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer()
+{
+    return (
+        <footer style={{color:"green"}}>
+        <p>© 2023 City Lovers</p>
+        </footer>
+    )
+    
+}
+ export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent()
+{
+    return (
+        <main style={{ color: "green"}}>       
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+
+}
+export default MainContent;
