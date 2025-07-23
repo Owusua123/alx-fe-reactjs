fc472beedff94e36224a5bb11fb462ed79391d2b
Nana Ama Sarpong <nanaamasarpong824@gmail.com>
2025-07-23 04:46:04 +0000

    Counter application

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-
+import Counter from './components/Counter.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,7 +18,8 @@ function App() {
         <WelcomeMessage />
         <Header />
         <MainContent />
-        <Footer/>
+        <Footer />
+        <Counter /> 
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
