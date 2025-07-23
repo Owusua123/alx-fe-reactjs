d6902b8089787415eb0226e9492f8fe8abfc2dc5
Nana Ama Sarpong <nanaamasarpong824@gmail.com>
2025-07-23 07:23:11 +0000

    Company Website with React

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import {Link} from react-router-dom
+
+function Navbar (){
+    return(
+        <nav style={{display:'flex'}}>
+            <Link stlye={{ color:'blue'}} to='/'>Home</Link>
+            <Link stlye={{ color:'blue'}} to='/about'>About</Link>
+            <Link stlye={{ color:'blue'}} to='/contact'>Contact</Link>
+            <Link stlye={{ color:'blue'}} to='/services'>Services</Link>
+
+        </nav>
+    )
+}
+export default Navbar
