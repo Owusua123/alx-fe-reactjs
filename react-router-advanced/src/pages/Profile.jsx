e666511bcf19549b9094967a413977164705596a
Nana Ama <wikiwoo@Nana-Amas-MacBook-Pro.local>
2025-08-30 00:22:05 +0000

    react router advanced git add .

--- react-router-advanced/src/pages/Profile.jsx
+++ react-router-advanced/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from "react-router-dom";
 export default function Profile() {
   return (
     <div>
-      <h1>👤 Profile</h1>
+      <h1>Profile</h1>
       <nav style={{ marginBottom: "1rem" }}>
         <Link to="details">Details</Link> |{" "}
         <Link to="settings">Settings</Link>
